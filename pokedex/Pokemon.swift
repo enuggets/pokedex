37d8e20b72f9727319c68efeb61bfc98fa69b1b4
Elodie Durouchoux <elodie.durouchoux@devialet.com>
2016-07-21 09:59:20 +0200

    Beginning the UI

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Elodie Durouchoux on 20/07/2016.
+//  Copyright © 2016 Elodie Durouchoux. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
